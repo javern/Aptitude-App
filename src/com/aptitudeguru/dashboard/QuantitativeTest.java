0b790adf9e671b6c76abd1f6bbe6ed19e7f11685
Colin <c.e.mackey@dundee.ac.uk>
2015-02-27 15:49:04 +0000

    Quantitative test (no tests)

---
+++ src/com/aptitudeguru/dashboard/QuantitativeTest.java
@@ -0,0 +1,20 @@
+package com.aptitudeguru.dashboard;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class QuantitativeTest {
+
+	//Could not find documentation on these methods to do these tests
+	@Test
+	public void testButtonCommon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStartActivityCommon() {
+		fail("Not yet implemented");
+	}
+
+}
