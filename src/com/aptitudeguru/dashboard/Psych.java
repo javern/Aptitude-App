04b26500e10d8fb2ca9767cbdb167dd445f2dd5f
kLundahl <kim.lundahl1@gmail.com>
2015-03-12 15:24:39 +0000

    Psych text

---
+++ src/com/aptitudeguru/dashboard/Psych.java
@@ -0,0 +1,14 @@
+package com.aptitudeguru.dashboard;
+
+import android.app.Activity;
+import android.os.Bundle;
+import androidhive.dashboard.R;
+
+public class Psych extends Activity {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.psych);
+	}
+
+	
+}
